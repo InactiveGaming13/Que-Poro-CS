2d262641d54f3499a6653dbe1f1400966e204753
InactiveGaming13 <116043268+InactiveGaming13@users.noreply.github.com>
2025-05-08 17:44:35 +1000

    Add summaries to TestersHandler.cs

--- Handlers/TestersHandler.cs
+++ Handlers/TestersHandler.cs
@@ -6,9 +6,16 @@ using DisCatSharp.Enums;
 
 namespace QuePoro.Handlers;
 
+/// <summary>
+/// The class for handling the tester commands.
+/// </summary>
 [SlashCommandGroup("testers", "Tester commands to check the bot is responding")]
 public class TesterCommands : ApplicationCommandsModule
 {
+    /// <summary>
+    /// A command that sends a test emoji.
+    /// </summary>
+    /// <param name="ctx">The context of the command.</param>
     [SlashCommand("emoji", "Sends the test emoji")]
     public async Task Emoji(InteractionContext ctx)
     {
@@ -17,6 +24,10 @@ public class TesterCommands : ApplicationCommandsModule
             .WithContent("<:test:1341993967046103113>"));
     }
     
+    /// <summary>
+    /// A command that sends a pong response.
+    /// </summary>
+    /// <param name="ctx">The context of the command.</param>
     [SlashCommand("ping", "Sends pong")]
     public async Task Ping(InteractionContext ctx)
     {
