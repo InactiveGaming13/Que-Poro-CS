09acc698f6bc8ab02ae18c5e209f48120777db0f
InactiveGaming13 <116043268+InactiveGaming13@users.noreply.github.com>
2025-06-25 18:34:06 +1000

    Added user config commands

--- Handlers/ConfigHandler.cs
+++ Handlers/ConfigHandler.cs
@@ -3,6 +3,8 @@ using DisCatSharp.ApplicationCommands.Attributes;
 using DisCatSharp.ApplicationCommands.Context;
 using DisCatSharp.Entities;
 using DisCatSharp.Enums;
+using QuePoro.Database.Handlers;
+using QuePoro.Database.Types;
 
 namespace QuePoro.Handlers;
 
@@ -10,7 +12,8 @@ namespace QuePoro.Handlers;
 public class ConfigCommands : ApplicationCommandsModule
 {
     [SlashCommand("response", "Sets weather or not the bot responds to you")]
-    public async Task Response(InteractionContext e, [Option("value", "True for response, false for silence")] bool silent = false)
+    public async Task Response(InteractionContext e, 
+        [Option("value", "True for response, false for silence")] bool respond = false)
     {
         await e.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
         if (e.Member?.VoiceState is null || e.Guild is null)
@@ -19,9 +22,50 @@ public class ConfigCommands : ApplicationCommandsModule
                 "I do not work in DMs."));
             return;
         }
-        await e.EditResponseAsync(new DiscordWebhookBuilder()
-            .WithContent(silent
-                ? "The bot will respond to your messages."
-                : "The bot will no longer respond to your messages."));
+        
+        if (!await Users.UserExists(e.UserId))
+            await Users.AddUser(e.UserId, e.User.Username, e.User.GlobalName);
+        UserRow user = await Users.GetUser(e.UserId);
+
+        if (respond.Equals(user.RepliedTo))
+        {
+            await e.EditResponseAsync(new DiscordWebhookBuilder().WithContent(
+                $"The bot already {(respond ? "responds" : "doesn't respond")} to you."));
+            return;
+        }
+
+        await Users.ModifyUser(e.UserId, e.User.GlobalName, repliedTo: respond);
+        
+        await e.EditResponseAsync(new DiscordWebhookBuilder().WithContent(
+            $"The bot will {(respond ? "now" : "no longer")} respond to your messages."));
+    }
+    
+    [SlashCommand("react", "Sets weather or not the bot replies to you")]
+    public async Task React(InteractionContext e, 
+        [Option("value", "True for response, false for silence")] bool react = false)
+    {
+        await e.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+        if (e.Member?.VoiceState is null || e.Guild is null)
+        {
+            await e.EditResponseAsync(new DiscordWebhookBuilder().WithContent(
+                "I do not work in DMs."));
+            return;
+        }
+
+        if (!await Users.UserExists(e.UserId))
+            await Users.AddUser(e.UserId, e.User.Username, e.User.GlobalName);
+        UserRow user = await Users.GetUser(e.UserId);
+
+        if (react.Equals(user.ReactedTo))
+        {
+            await e.EditResponseAsync(new DiscordWebhookBuilder().WithContent(
+                $"The bot already {(react ? "reacts" : "doesn't react")} to you."));
+            return;
+        }
+
+        await Users.ModifyUser(e.UserId, e.User.GlobalName, reactedTo: react);
+        
+        await e.EditResponseAsync(new DiscordWebhookBuilder().WithContent(
+                $"The bot will {(react ? "now" : "no longer")} react to your messages."));
     }
 }
